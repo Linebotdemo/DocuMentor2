20af20e9f7f087585e490903e6a59de7fcd801dd
Linebotdemo <k.inose0902@gmail.com>
2025-04-10 08:20:49 +0900

    initial commit

--- worker.py
+++ worker.py
@@ -1,19 +1,17 @@
+# tasks.py
+
 import os
-from celery import Celery
 import requests
+from celery import Celery
 from dotenv import load_dotenv
 
-# .env 読み込み
 load_dotenv()
 
-# 環境変数から設定取得
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 WHISPER_API_URL = os.getenv("WHISPER_API_URL", "http://localhost:5000/transcribe")
 
-# Celery設定
 celery = Celery("documentor_worker", broker=REDIS_URL, backend=REDIS_URL)
 
-# タスク定義
 @celery.task
 def transcribe_video_task(video_url, video_id):
     print(f"🎬 Transcribing video {video_id} from {video_url}")
